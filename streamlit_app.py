fd186d4ae4cc100593fc3d967a941cc97cd8f233
rjarupula1729 <jarupularavi1729@gmail.com>
2025-07-16 18:27:43 +0530

    Update streamlit_app.py
    Updated with API URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,5 @@ if time_to_insert:
 
 
 import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon", timeout = 10)
+smoothiefroot_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
