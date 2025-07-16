51e71d4c427d8bbd9e0fc8cfff717ef62119d3eb
rjarupula1729 <jarupularavi1729@gmail.com>
2025-07-16 22:13:27 +0530

    Update streamlit_app.py
    adding another column in multiple selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,9 @@ st.write('Tha name on your smoothie will be: ', name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data = my_dataframe, use_container_width = True)
+st.stop()
 
 ingredient_list = st.multiselect(
     'Choose up to 5 ingredients:', my_dataframe ,max_selections=5)
